97386b982ef26e64a1659a3cacd94dd733e0fbf9
bpbyn <punongbayan.bri@gmail.com>
2022-10-16 12:13:08 +0800

    Initialize Firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import React, { useRef } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 import { Hydrate } from 'react-query/hydration';
 
 import theme from '../theme';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps: p }: AppProps) => {
   const queryClientRef = useRef<QueryClient>();
+  // workaround for current bug in Next.js
+  const pageProps: any = p;
+
   if (!queryClientRef.current) {
     queryClientRef.current = new QueryClient();
   }
@@ -20,7 +23,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </ChakraProvider>
       </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };
